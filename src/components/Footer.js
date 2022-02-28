61deef45442a4472432bcf1447931b30ada01008
Austin Pratt <hargis32@gmail.com>
2022-02-28 13:58:04 -0600

    updated footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer>
+            <ul>
+                <li>
+                    <a href="https://github.com/hargis32">Github</a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/austin-pratt-887273206/">LinkedIn</a>
+                </li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer
