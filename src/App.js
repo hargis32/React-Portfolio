5617180a4fd6d210d2c7bc5b496248f9d4d77188
Austin Pratt <hargis32@gmail.com>
2022-02-28 13:45:49 -0600

    setup app.js to accept components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
-import BucketList from './components/BucketList';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import NavTabs from './components/NavTabs';
+import PortfolioContainer from './components/PortfolioContainer';
 
 function App() {
   return (
-    <div className="bucket-app">
-      <BucketList />
+    <div className="portfolio-app">
+      <Header>
+        <PortfolioContainer></PortfolioContainer>
+        <Footer></Footer>
+        </Header>
     </div>
   );
 }
